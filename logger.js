b77eb1a94cfd57c1c9dd0a195b5870b656857cc6
alfazsozib <alfazsozib@gmail.com>
2024-11-06 00:50:33 +0600

    done

--- logger.js
+++ logger.js
@@ -25,6 +25,7 @@ logger.on('message',logToFile)
 setInterval(()=>{
     const memoryUsage = (os.freemem() / os.totalmem) * 100;
     logger.log(`Current memory usage: ${memoryUsage.toFixed(2)}`)
+ 
 },3000)
 
 
